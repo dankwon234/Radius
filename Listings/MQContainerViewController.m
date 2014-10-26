5f3388884a93c1db68c27cc35e4b4d7bdf57e140
Dan Kwon <dankwon@dans-air.home>
2014-10-25 22:22:41 -0400

    register for toggle menu notification. toggle menu in and out

--- Listings/MQContainerViewController.m
+++ Listings/MQContainerViewController.m
@@ -24,6 +24,12 @@
     if (self) {
         self.listings = [NSMutableArray array];
         
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(toggleMenu)
+                                                     name:@"ViewMenuNotification"
+                                                   object:nil];
+
+        
     }
     return self;
 }
@@ -45,10 +51,38 @@
 }
 
 
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+}
+
+- (void)toggleMenu
+{
+//    NSLog(@"toggle menu: %.2f", );
+    [UIView animateWithDuration:0.70f
+                          delay:0
+         usingSpringWithDamping:0.5f
+          initialSpringVelocity:0.0f
+                        options:UIViewAnimationOptionCurveEaseInOut
+                     animations:^{
+                         CGRect frame = self.view.frame;
+                         CGFloat halfWidth = 0.50f*frame.size.width;
+                         CGPoint center = self.navCtr.view.center;
+                         
+                         if (center.x==halfWidth){
+                             center.x = 0.95f*frame.size.width;
+                         }
+                         else{
+                             center.x = halfWidth;
+                         }
+                         
+                         
+                         self.navCtr.view.center = center;
+                     }
+                     completion:^(BOOL finished){
+                         
+                     }];
+    
 }
 
 - (void)didReceiveMemoryWarning

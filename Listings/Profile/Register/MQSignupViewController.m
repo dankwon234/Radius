05153b2f04d33a86393b3285184d4076a56d7ee3
Dan Kwon <dankwon@dans-air.home>
2014-10-25 21:01:17 -0400

    removed commented out code

--- Listings/Profile/Register/MQSignupViewController.m
+++ Listings/Profile/Register/MQSignupViewController.m
@@ -64,22 +64,6 @@
     }
     
     CGFloat padding = 12.0f;
-    
-//    UIButton *btnNext = [UIButton buttonWithType:UIButtonTypeCustom];
-//    btnNext.frame = CGRectMake(padding, frame.size.height-h-padding, frame.size.width-2*padding, h);
-//    btnNext.autoresizingMask = UIViewAutoresizingFlexibleTopMargin;
-//    btnNext.backgroundColor = [UIColor clearColor];
-//    btnNext.layer.borderColor = [[UIColor whiteColor] CGColor];
-//    btnNext.layer.borderWidth = 1.5f;
-//    btnNext.layer.cornerRadius = 4.0f;
-//    btnNext.layer.masksToBounds = YES;
-//    btnNext.titleLabel.font = [UIFont fontWithName:@"Heiti SC" size:16.0f];
-//    [btnNext setTitle:@"NEXT" forState:UIControlStateNormal];
-//    [btnNext setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-//    [btnNext addTarget:self action:@selector(btnNextAction:) forControlEvents:UIControlEventTouchUpInside];
-//    [view addSubview:btnNext];
-
-    
     UIView *next = [[UIView alloc] initWithFrame:CGRectMake(0.0f, frame.size.height-64.0f, frame.size.width, 64.0f)];
     next.backgroundColor = [UIColor grayColor];
     next.autoresizingMask = UIViewAutoresizingFlexibleTopMargin;

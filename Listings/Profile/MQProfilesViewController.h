feb40e5e6390210ca6a3d4964c70bf8c1b08790a
Dan Kwon <dankwon@dans-air.home>
2014-10-27 04:38:40 -0400

    set up empty view controller for browsing profiles

---
+++ Listings/Profile/MQProfilesViewController.h
@@ -0,0 +1,13 @@
+//
+//  MQProfilesViewController.h
+//  Listings
+//
+//  Created by Dan Kwon on 10/27/14.
+//  Copyright (c) 2014 Mercury. All rights reserved.
+//
+
+#import "MQViewController.h"
+
+@interface MQProfilesViewController : MQViewController
+
+@end
---
+++ Listings/Profile/MQProfilesViewController.m
@@ -0,0 +1,68 @@
+//
+//  MQProfilesViewController.m
+//  Listings
+//
+//  Created by Dan Kwon on 10/27/14.
+//  Copyright (c) 2014 Mercury. All rights reserved.
+
+
+#import "MQProfilesViewController.h"
+
+@interface MQProfilesViewController ()
+
+@end
+
+@implementation MQProfilesViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+
+    }
+    return self;
+    
+}
+
+- (void)loadView
+{
+    UIView *view = [self baseView:YES];
+//    CGRect frame = view.frame;
+    
+    UIImage *bgImage = [UIImage imageNamed:@"bgBlurry1.png"];
+    view.backgroundColor = [UIColor colorWithPatternImage:bgImage];
+
+    
+    self.view = view;
+}
+
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.navigationItem.hidesBackButton = YES;
+    
+    UIImage *imgHamburger = [UIImage imageNamed:@"iconHamburger.png"];
+    UIButton *btnMenu = [UIButton buttonWithType:UIButtonTypeCustom];
+    btnMenu.frame = CGRectMake(0.0f, 0.0f, 0.5f*imgHamburger.size.width, 0.5f*imgHamburger.size.height);
+    [btnMenu setBackgroundImage:imgHamburger forState:UIControlStateNormal];
+    [btnMenu addTarget:self action:@selector(viewMenu:) forControlEvents:UIControlEventTouchUpInside];
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:btnMenu];
+}
+
+- (void)viewMenu:(id)sender
+{
+    NSLog(@"view menu");
+    [[NSNotificationCenter defaultCenter] postNotification:[NSNotification notificationWithName:kViewMenuNotification object:nil]];
+}
+
+
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+
+@end

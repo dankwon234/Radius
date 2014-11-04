dbfc8edcf811c9f37a4f14edcf6c2b3117cdac6b
Dan Kwon <dankwon@dans-air.home>
2014-11-04 00:33:06 -0500

    set up references table with custom cells

--- Listings/Profile/MQReferencesViewController.m
+++ Listings/Profile/MQReferencesViewController.m
@@ -8,9 +8,11 @@
 
 #import "MQReferencesViewController.h"
 #import "MQContactsViewController.h"
+#import "MQWebServices.h"
+#import "MQReferenceCell.h"
 
 @interface MQReferencesViewController ()
-@property (strong, nonatomic) UITableView *contactsTable;
+@property (strong, nonatomic) UITableView *referencesTable;
 @end
 
 @implementation MQReferencesViewController
@@ -36,11 +38,18 @@
     UIImage *bgImage = [UIImage imageNamed:@"bgLegsBlue.png"];
     view.backgroundColor = [UIColor colorWithPatternImage:bgImage];
     
-    self.contactsTable = [[UITableView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, frame.size.width, frame.size.height) style:UITableViewStylePlain];
-    self.contactsTable.autoresizingMask = (UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleHeight);
-    self.contactsTable.dataSource = self;
-    self.contactsTable.delegate = self;
-    [view addSubview:self.contactsTable];
+    UIView *line = [[UIView alloc] initWithFrame:CGRectMake(36.0f, 0.0f, 2.0f, frame.size.height)];
+    line.autoresizingMask = (UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleHeight);
+    line.backgroundColor = [UIColor whiteColor];
+    [view addSubview:line];
+    
+    self.referencesTable = [[UITableView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, frame.size.width, frame.size.height) style:UITableViewStylePlain];
+    self.referencesTable.autoresizingMask = (UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleHeight);
+    self.referencesTable.dataSource = self;
+    self.referencesTable.delegate = self;
+    self.referencesTable.separatorStyle = UITableViewCellSelectionStyleNone;
+    self.referencesTable.backgroundColor = [UIColor clearColor];
+    [view addSubview:self.referencesTable];
 
     self.view = view;
 }
@@ -53,6 +62,46 @@
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd
                                                                                            target:self
                                                                                            action:@selector(addReference:)];
+    
+    if (self.profile.references)
+        return;
+    
+    [self.loadingIndicator startLoading];
+    [[MQWebServices sharedInstance] fetchReferences:self.profile completion:^(id result, NSError *error){
+        [self.loadingIndicator stopLoading];
+        
+        if (error){
+            [self showAlertWithtTitle:@"Error" message:[error localizedDescription]];
+            return;
+        }
+        
+        NSDictionary *results = (NSDictionary *)result;
+        NSLog(@"%@", [results description]);
+        
+        NSArray *r = results[@"references"];
+        self.profile.references = [NSMutableArray array];
+        for (int i=0;i<r.count; i++) {
+            NSMutableDictionary *reference = [NSMutableDictionary dictionaryWithDictionary:r[i]];
+            
+            NSArray *parts = [reference[@"timestamp"] componentsSeparatedByString:@" "];
+            if (parts.count > 5){
+                NSString *month = parts[1];
+                NSString *day = parts[2];
+                NSString *year = parts[parts.count-1];
+                
+                if ([day hasPrefix:@"0"])
+                    day = [day stringByReplacingOccurrencesOfString:@"0" withString:@""];
+                
+                reference[@"formattedDate"] = [NSString stringWithFormat:@"%@ %@, %@", month, day, year];
+            }
+
+            [self.profile.references addObject:reference];
+        }
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self.referencesTable reloadData];
+        });
+    }];
 
 }
 
@@ -76,18 +125,31 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 30;
+    return self.profile.references.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     static NSString *cellId = @"cellId";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId];
+    MQReferenceCell *cell = (MQReferenceCell *)[tableView dequeueReusableCellWithIdentifier:cellId];
     if (cell==nil)
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellId];
+        cell = [[MQReferenceCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellId];
     
-    cell.textLabel.text = [NSString stringWithFormat:@"%d", (int)indexPath.row];
+    NSDictionary *reference = (NSDictionary *)self.profile.references[indexPath.row];
+    cell.lblText.text = reference[@"text"];
+    cell.lblDate.text = reference[@"formattedDate"];
     return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSDictionary *reference = (NSDictionary *)self.profile.references[indexPath.row];
+    CGRect boundingRect = [reference[@"text"] boundingRectWithSize:CGSizeMake([MQReferenceCell textLabelWidth], 250.0f)
+                                                           options:NSStringDrawingUsesLineFragmentOrigin
+                                                        attributes:@{NSFontAttributeName:[MQReferenceCell textLabelFont]}
+                                                           context:nil];
+    
+    return boundingRect.size.height+44.0f;
 }
 
 

b9739c78f5746bc10e05943af2dd4fe3e711cfdd
Dan Kwon <dankwon@dans-air.home>
2014-11-09 00:11:11 -0500

    added resume change functionality to resume view

--- Listings/Profile/MQResumeViewController.m
+++ Listings/Profile/MQResumeViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "MQResumeViewController.h"
+#import "MQWebServices.h"
 
 @interface MQResumeViewController ()
 @property (strong, nonatomic) UIWebView *pdfWebview;
@@ -33,6 +34,7 @@
     self.pdfWebview = [[UIWebView alloc] initWithFrame:frame];
     self.pdfWebview.autoresizingMask = (UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleHeight);
     self.pdfWebview.delegate = self;
+    self.pdfWebview.scalesPageToFit = YES;
     [view addSubview:self.pdfWebview];
 
     self.view = view;
@@ -41,8 +43,40 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    NSString *url = [kBaseUrl stringByAppendingString:[NSString stringWithFormat:@"pdf/%@", self.profile.resume]];
+    [self addCustomBackButton];
+    
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Change"
+                                                                              style:UIBarButtonItemStylePlain
+                                                                             target:self
+                                                                             action:@selector(changeResume:)];
+    
+    NSString *url = [kBaseUrl stringByAppendingString:[NSString stringWithFormat:@"site/pdf/%@", self.profile.resume]];
     [self.pdfWebview loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:url]]];
+}
+
+- (void)changeResume:(UIBarButtonItem *)btn
+{
+    
+    [self.loadingIndicator startLoading];
+    [[MQWebServices sharedInstance] resumeRequest:self.profile completionBlock:^(id result, NSError *error){
+        [self.loadingIndicator stopLoading];
+        
+        if (error){
+            [self showAlertWithtTitle:@"Error" message:[error localizedDescription]];
+            return;
+        }
+        
+        NSDictionary *results = (NSDictionary *)result;
+        NSLog(@"%@", [results description]);
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSString *msg = [NSString stringWithFormat:@"We sent a link to %@.\n\nTo upload your resume, click the link and login with your email and password.", [self.profile.email uppercaseString]];
+            [self showNotification:@"Upload Resume" withMessage:msg];
+        });
+    }];
+    
+
+    
 }
 
 
